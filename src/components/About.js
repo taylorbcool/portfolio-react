3b70ed1c5a8a06ea067ce40c4d1803145f714ab9
Taylor Cool <taycool@gmail.com>
2019-11-30 15:35:34 -0800

    added styles and fleshed out pages

--- src/components/About.js
+++ src/components/About.js
@@ -3,10 +3,12 @@ import '../styles/About.scss'
 
 const About = () =>{
     return (
-        <div className='card'>
-            <h3>About Taylor</h3>
-            <p>Born and raised in Casper, Wyoming, I like to bring the "small town" culture in which I was raised to all of my work. Treating people with respect, taking pride in your work, and a strong committment to success are the values that guide me. I graduated with an A.A. in Multimedia Communications in May of 2019. After graduation I moved to Las Vegas to soak up the thriving culture and arts scene, and continue my education in Web Development.</p>
-            <p>Web development and design are not my only passions in life. I am also an avid follower of sports. My favorite teams are the Los Angeles Lakers, the Philadelphia Eagles, and the Atlanta Braves. Growing up in Wyoming I never had any local professional sports teams to root for, so I had to pick from the ones I could see on TV. I played baseball until college, but recently discovered an intense love of golf. These days almost all of my free time is spent on the links, which works out well in Las Vegas. I find the golf course is a great place to unwind and let my mind wander, which often helps me in my professional life. In addition to my addiction to golf, I also have a fantasy football podcast.</p>
+        <div className='container'>
+            <div className='card'>
+                <h3>About Taylor</h3>
+                <p>Born and raised in Casper, Wyoming, I like to bring the "small town" culture in which I was raised to all of my work. Treating people with respect, taking pride in your work, and a strong committment to success are the values that guide me. I graduated with an A.A. in Multimedia Communications in May of 2019. After graduation I moved to Las Vegas to soak up the thriving culture and arts scene, and continue my education in Web Development.</p>
+                <p>Web development and design are not my only passions in life. I am also an avid follower of sports. My favorite teams are the Los Angeles Lakers, the Philadelphia Eagles, and the Atlanta Braves. Growing up in Wyoming I never had any local professional sports teams to root for, so I had to pick from the ones I could see on TV. I played baseball until college, but recently discovered an intense love of golf. These days almost all of my free time is spent on the links, which works out well in Las Vegas. I find the golf course is a great place to unwind and let my mind wander, which often helps me in my professional life. In addition to my addiction to golf, I also have a fantasy football podcast.</p>
+            </div>
         </div>
     )
 }
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import '../styles/Contact.scss';
 
 const Contact = () =>{
-    return null;
+    return(
+        <div className='container'>
+            <div className='contact'>
+                <h3>Contact Taylor</h3>
+                <p>Have a project you'd like to collaborate on? Let's talk:</p>
+                <p>Email: taylor.bryce.cool@gmail.com</p>
+            </div>
+        </div>
+    )
 }
 
 export default Contact;
